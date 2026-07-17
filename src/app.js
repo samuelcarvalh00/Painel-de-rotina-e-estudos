e83aaaa2ce5ff93935b7cdc6c9ca41ac237649da
samuelcarvalh00 <seuemail@example.com>
2026-07-17 13:34:02 -0300

    função para tem cards de progresso

--- src/app.js
+++ src/app.js
@@ -12,27 +12,45 @@ function renderizarcards(listadetarefas = taskslist
         return
     }
 
-    listadetarefas.forEach(task => {
-        const card = document.createElement('div')
-        card.className = 'card-tarefa'
-        card.innerHTML = `<div class="card-header">
-                <h3>${task.name}</h3>
-                <span class="badge-alta">${task.priority}</span>
-            </div>
-            <p class="card-info"><strong>Tecnologia:</strong> ${task.tech}</p>
-            <p class="card-info"><strong>Horas:</strong> ${task.hours}h</p>
-            <div class="card-footer">
-                <span class="badge-fazendo">${task.status}</span>
-                <button onclick="deletartarefa(${task.id})" class="text-red-600 hover:text-red-800 text-sm cursor-pointer">Excluir</button>
-            </div>`
-            cardsgrid.appendChild(card)
-    });
+   listadetarefas.forEach(task => {
+    const card = document.createElement('div')
+    card.className = 'card-tarefa'
+    
+    // Criamos as opções do select checando qual é o status atual da tarefa para vir marcado (selected)
+    card.innerHTML = `<div class="card-header">
+            <h3>${task.name}</h3>
+            <span class="badge-alta">${task.priority}</span>
+        </div>
+        <p class="card-info"><strong>Tecnologia:</strong> ${task.tech}</p>
+        <p class="card-info"><strong>Horas:</strong> ${task.hours}h</p>
+        <div class="card-footer">
+            <select onchange="atualizarstatustarefa(${task.id}, this.value)" class="select-status-card">
+                <option value="pendente" ${task.status === 'pendente' ? 'selected' : ''}>Pendente</option>
+                <option value="fazendo" ${task.status === 'fazendo' ? 'selected' : ''}>Fazendo</option>
+                <option value="feito" ${task.status === 'feito' ? 'selected' : ''}>Feito</option>
+            </select>
+            <button onclick="deletartarefa(${task.id})" class="text-red-600 hover:text-red-800 text-sm cursor-pointer">Excluir</button>
+        </div>`
+        cardsgrid.appendChild(card)
+});
 }
 
 window.deletartarefa = function(id){
     taskslist = taskslist.filter(task => task.id !== id)
     localStorage.setItem('tasks', JSON.stringify(taskslist))
     renderizarcards()
+    atualizarpainelprogresso()
+}
+
+window.atualizarstatustarefa = function(id, novostatus){
+    taskslist = taskslist.map(task => {
+        if (task.id === id) {
+            return { ...task, status: novostatus}
+        }
+        return task
+    })
+    localStorage.setItem('tasks', JSON.stringify(taskslist))
+    atualizarpainelprogresso()
 }
 
 
@@ -61,7 +79,7 @@ const taskhours = document.getElementById('task-hours').value
 
  taskform.reset()
 renderizarcards()
-
+atualizarpainelprogresso()
 alert("Tarefa adicionada com sucesso", newtask)
 
 })
@@ -83,8 +101,6 @@ let aplicarfiltros = () => {
                 return batetech && batestatus && batepriority
             })
 
-
-renderizarcards(tarefasfiltradas)
 }
 
 const btnfiltrar = document.getElementById('btn-filtro')
@@ -116,3 +132,49 @@ if(temasalvo){
 
 btnlight.addEventListener('click', () => aplicartema('light'))
 btndark.addEventListener('click', () => aplicartema('dark'))
+
+function atualizarpainelprogresso(){
+    const pgeral = document.getElementById('progresso-geral')
+    const divhorastech = document.getElementById('horas-tecnologia')
+    const pPrioridades = document.getElementById('total-prioridades')
+
+    const total = taskslist.length
+
+    if(total === 0){
+        pgeral.textContent = "Total: 0 | Feitas 0 (0%)"
+        divhorastech.innerHTML = "<p class'text-gray-400'>Nenhum hora registrada.</p>"
+        pPrioridades.textContent = "Alta: 0 | Média: 0 | Baixa: 0"
+        return
+    }
+
+    const feitas = taskslist.filter(task => task.status === 'feito').length
+    const porcentagem = Math.round((feitas/total) * 100)
+    pgeral.textContent = `Total: ${total} | Feitas ${feitas} (${porcentagem}%)`
+
+    const horasagrupadas = taskslist.reduce((acumulador, task) => {
+        const tech = task.tech.toUpperCase()
+        const horas = Number(task.hours) || 0
+
+        if(!acumulador[tech]){
+            acumulador[tech] = 0
+        }
+
+        acumulador[tech] +=horas
+        return acumulador
+}, {})
+
+divhorastech.innerHTML = Object.entries(horasagrupadas).map(([tech,horas]) => `<p><strong>${tech}:</strong> ${horas}h</p>`).join('')
+
+
+
+let alta = taskslist.filter(task => task.priority === 'high' || task.priority === 'alta').length
+let media = taskslist.filter(task => task.priority === 'medium' || task.priority === 'media').length
+let baixa = taskslist.filter(task => task.priority === 'low' || task.priority === 'baixa').length
+
+pPrioridades.textContent = `Alta: ${alta} | Média: ${media} | Baixa: ${baixa}`
+
+}
+
+
+renderizarcards(tarefasfiltradas)
+atualizarpainelprogresso()
